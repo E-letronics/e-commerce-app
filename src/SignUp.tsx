186d09a059261478fa63b498bbc52ed271ed40d1
Beatriz Assumção da Silva <b.assumcao.da.silva@accenture.com>
2023-06-01 11:47:30 -0300

    feat: make a signup request to a fake api

--- src/SignUp.tsx
+++ src/SignUp.tsx
@@ -1,9 +1,13 @@
 import styles from './SignUp.module.css';
 import { useState } from 'react';
 import { SignUpFormDto } from './dto/SignUpEventDto';
+import axios from 'axios';
 
 function SignUp() {
   const [signupForm, setSignupForm] = useState<SignUpFormDto>({});
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [response, setResponse] = useState("");
 
   function handleInputChange(event: any) {
     setSignupForm((p) => ({
@@ -13,7 +17,24 @@ function SignUp() {
   }
 
   function handleSubmit(event: any) {
+    setEmail(signupForm.email!);
+    setPassword(signupForm.password!);
     event.preventDefault();
+
+    axios({
+      method: 'POST',
+      baseURL: 'http://api.fakeshop-api.com',
+      url: '/users/signup',
+      data: {
+        email,
+        password,
+      },
+    })
+    .then(({data}: any) => {
+      setResponse(data);
+      localStorage.setItem('token', data.token);
+    })
+    .catch((err:any) => console.dir(err))
   }
 
   return (
